5721ff7b69624b273e9edc4a514727aade1f8375
unknown <samsiggs00@gmail.com>
2019-08-23 07:28:38 +1200

    First Commit

---
+++ src/test/java/tut4/TestCalc.java
@@ -0,0 +1,39 @@
+package tut4;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class TestCalc 
+    extends TestCase
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public TestCalc( String testName )
+    {
+        super( testName );
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( AppTest.class );
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testApp()
+    {
+        Calc c = new Calc();
+        assertTrue(c.add(2,4) == 4);
+    }
+}
